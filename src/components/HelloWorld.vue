d33c58b8dab84c494b63ac9297b20c9fd5a5f94a
ARSLAN ASLAM <beingfastian@gmail.com>
2022-12-28 02:28:06 +0500

    Hi, here is the resolved issue.
    I made the following changes to your code:
    
    - Imported the ref and computed functions from the vue module.
    - Declared url as a computed property, which is a function that returns the URL string that concatenates the 'https://80fb-31-187-152-178.ngrok.io/openai?q=' string with the value of query. The computed function helps to ensure that the url value is automatically updated whenever the query value changes.
    With these changes, the url value will be automatically updated whenever the query value changes, and the url value can be accessed and rendered in the template using {{ url }}.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <template>
- <div>
-  <label>Type you query:</label>
-      <input type="text" class="form-control" v-model="query">
-      <p>user typed: query:  {{ query }}</p>
-       <p>url with the users query as url variable  {{ url }}</p> 
-   </div>  
+  <div>
+    <label>Type you query:</label>
+    <input type="text" class="form-control" v-model="query" />
+    <p>user typed: query: {{ query }}</p>
+    <p>url with the users query as url variable {{ url }}</p>
+  </div>
 </template>
-<script setup>  
-import { ref} from "vue"
-const query = ref('defaultvalue')
-const url = "https://80fb-31-187-152-178.ngrok.io/openai?q=" + query.value
+<script setup>
+import { ref, computed } from "vue";
+const query = ref("defaultvalue");
+const url = computed(() => {
+  return "https://80fb-31-187-152-178.ngrok.io/openai?q=" + query.value;
+});
 </script>
